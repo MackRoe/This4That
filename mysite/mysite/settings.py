ecd2c94ebe4cf615ace7ee0fc2de27c22e163d2e
MackRoe <elaine.music@students.makeschool.com>
2019-12-13 12:44:15 -0800

    add allowed hosts. 4th deployment attempt

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iyavx77%&eb4ad)%@d723=7tdye3yu2un(=n@d1$yi41e@c3(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'tradeit-em-v2.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'tradeit-em-v2.herokuapp.com', 'tradeit-em.herokuapp.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/templates/static/'
 
 # Extra places for collectstatic to find static files.
